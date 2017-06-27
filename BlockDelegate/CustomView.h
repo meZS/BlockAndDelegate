8b925f2f5b0c037701e240a8b5be10c45ac9ea22
zs <798506095@qq.com>
2017-06-27 16:16:29 +0800

    block 与 代理的关系

---
+++ BlockDelegate/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  BlockDelegate
+//
+//  Created by wordy on 2017/6/27.
+//  Copyright © 2017年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ BlockDelegate/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  BlockDelegate
+//
+//  Created by wordy on 2017/6/27.
+//  Copyright © 2017年 golddatacommunications. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
